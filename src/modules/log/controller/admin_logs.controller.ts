54ca1331c7f33c0775e48afe186a3ba4c75e576b
mhmad-kavosi <mhmad.kavosi@gmail.com>
2025-02-22 15:54:55 +0330

    add get log for all

--- src/modules/log/controller/admin_logs.controller.ts
+++ src/modules/log/controller/admin_logs.controller.ts
@@ -42,3 +42,45 @@ export const get_all = async (req: Request, res: Response) => {
 		data: result.data
 	});
 };
+
+export const get_all_for_users = async (req: Request, res: Response) => {
+	const validate = new Validator(
+		{
+			page: req.query.page,
+			limit: req.query.limit,
+			type: req.query.type,
+			start_date: req.query.start_date,
+			end_date: req.query.end_date,
+			user_type: req.query.user_type,
+			user_id: req.query.user_id
+		},
+		{
+			page: ['required', 'numeric'],
+			limit: ['required', 'numeric'],
+			type: ['string', { in: Object.keys(LogTypeEnum) }],
+			start_date: ['string'],
+			end_date: ['string'],
+			user_type: ['string', { in: ['teacher', 'student'] }, 'required'],
+			user_id: ['string']
+		}
+	);
+
+	if (validate.fails()) {
+		return new PreconditionFailedError(res, validate.errors.all());
+	}
+
+	const result = await new LogsInfo().get_all(
+		<string>req.query.user_type,
+		Number(req.query.page),
+		Number(req.query.limit),
+		<string>req.query.type,
+		<string>req.query.start_date,
+		<string>req.query.end_date,
+		<string>req.query.user_id
+	);
+
+	return ApiRes(res, {
+		status: result.is_success ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR,
+		data: result.data
+	});
+};
--- src/modules/log/routes/admin_logs.routes.ts
+++ src/modules/log/routes/admin_logs.routes.ts
@@ -7,4 +7,6 @@ const AdminLogsRouter: Router = Router();
 const route_prefix = '/admin/logs';
 
 AdminLogsRouter.get(`${route_prefix}`, AdminAuthMiddleware, AdminLogsController.get_all);
+AdminLogsRouter.get(`${route_prefix}/users`, AdminAuthMiddleware, AdminLogsController.get_all_for_users);
+
 export default AdminLogsRouter;
